6f26d60796161a1372945efee10792866f76eb7e
Max Kivits <maxkivits42@gmail.com>
2022-05-11 08:32:22 +0200

    added some helpers

--- main.cpp
+++ main.cpp
@@ -17,15 +17,38 @@ void qDebugVector(std::vector<T>& vector) {
         qDebug() << el;
     }
 }
+template<typename T>
+QString QVectorToQString(QVector<T> InputVector) {
+    QString Output = "";
+    for(const auto& el: InputVector) {
+        Output += QString(el) + " ";
+    }
+    return Output;
+}
+template <>
+QString QVectorToQString<int>(QVector<int> InputVector) {
+    QString Output = "";
+    for(const auto& el: InputVector) {
+        Output += QString::number(el) + " ";
+    }
+    return Output;
+}
+
+// template<typename T>
+// QString QVectorToQString(QVector<T> InputVector) {
+//     QString OutputQString = "";
+//     std::accumulate(InputVector.begin(),InputVector.end(),OutputQString.begin(),"");
+//     return OutputQString;
+// }
 
 int main(int argc, char* argv[]) {
 
     std::vector<size_t> v {2,4,6,8,10,12,14,16,18,20};
-    std::vector<size_t> r {2,4,6,8,10,12,14,16,18,20};
 
-    std::adjacent_difference(v.begin(), v.end(), r.begin());
+    auto i = std::adjacent_find(v.begin(), v.end(), [](const auto & left, const auto & right){return left > right;});
+    std::cout << *i;
 
-    printVector(r);
-    
+    QMap<int, int> InputTypeIndex2StackedWidgetIndexMap{{1,1},{2,1},{3,1},{4,2}};	//Maps input type combobox index to corresponding stackedWidget pages
+
     return 0;
 }
